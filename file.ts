78838c41faf77b1b8f10f36bf3e1c617bbc5f71c
Alexander <lsndrme@ya.ru>
2024-04-12 20:16:09 +0700

    feat: implement rule

---
+++ file.ts
@@ -0,0 +1,2 @@
+// This file is required by RuleTester for testing typed rules
+// See https://typescript-eslint.io/developers/custom-rules/#testing-typed-rules
---
+++ src/rule.ts
@@ -0,0 +1,55 @@
+import { ESLintUtils } from '@typescript-eslint/utils';
+import { ClassType } from './class-type';
+
+const createRule = ESLintUtils.RuleCreator(
+    () => `https://github.com/lsndr/eslint-plugin-typescript-no-inheritance`,
+);
+
+export const rule = createRule({
+    create(context, options) {
+        return {
+            ClassDeclaration(node) {
+                const services = ESLintUtils.getParserServices(context);
+                const tsNode = services.esTreeNodeToTSNodeMap.get(node);
+                const checker = services.program.getTypeChecker();
+
+                const classType = new ClassType(tsNode, checker);
+
+                if (!classType.hasInheritance()) {
+                    return;
+                }
+
+                if (options[0]?.noInheritanceOfAbstractClasses || !classType.inheritsOnlyAbstractClasses()) {
+                    context.report({
+                        messageId: 'inheritanceNotAllowed',
+                        node,
+                    });
+                }
+            },
+        };
+    },
+    meta: {
+        docs: {
+            description: 'Inheritance is not allowed.',
+        },
+        messages: {
+            inheritanceNotAllowed: 'Inheritance is not allowed.',
+        },
+        type: 'problem',
+        schema: [
+            {
+                type: 'object',
+                properties: {
+                    noInheritanceOfAbstractClasses: {
+                        type: 'boolean',
+                    },
+                },
+                additionalProperties: false,
+            }
+        ],
+    },
+    name: 'no-inheritance',
+    defaultOptions: [{
+        noInheritanceOfAbstractClasses: false
+    }],
+});
