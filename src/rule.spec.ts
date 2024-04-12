1fc2cc6823fffa2f0b28dc36930425ec474f594a
Alexander <lsndrme@ya.ru>
2024-04-12 20:16:25 +0700

    test: add test cases

---
+++ src/rule.spec.ts
@@ -0,0 +1,43 @@
+import { RuleTester } from '@typescript-eslint/rule-tester';
+import { rule } from './rule';
+import * as path from 'path';
+
+describe('Rule', () => {
+    const tester = new RuleTester({
+        parser: '@typescript-eslint/parser',
+        parserOptions: {
+            project: './tsconfig.json',
+            tsconfigRootDir: path.resolve(__dirname, '..')
+        }
+    });
+
+    tester.run('no-inheritance', rule, {
+        valid: [
+            'abstract class Grandparent {} abstract class Parent extends Grandparent {} class Child extends Parent {}',
+            'abstract class Parent {} class Child extends Parent {}',
+            'interface Type {} class Child implements Type {}',
+            'interface Type {} abstract class Parent {} class Child extends Parent implements Type {}',
+            {
+                code: 'abstract class Parent {} class Child extends Parent {}',
+                options: [{ noInheritanceOfAbstractClasses: false }]
+            }
+        ],
+        invalid: [
+            {
+                code: 'class Parent {} class Child extends Parent {}',
+                errors: [{ messageId: 'inheritanceNotAllowed' }],
+            },
+            {
+                code: 'abstract class Parent {} class Child extends Parent {}',
+                errors: [{ messageId: 'inheritanceNotAllowed' }],
+                options: [{ noInheritanceOfAbstractClasses: true }]
+            },
+            {
+                code: 'class Grandparent {} abstract class Parent extends Grandparent {} class Child extends Parent {}',
+                errors: [{ messageId: 'inheritanceNotAllowed' }],
+            },
+        ],
+    });
+});
+
+
