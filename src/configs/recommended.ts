84a14b7a914a9e542723b9455d1040e21aba074c
Alexander <lsndrme@ya.ru>
2024-04-15 22:48:16 +0700

    refactor: rename rule

--- src/configs/recommended.ts
+++ src/configs/recommended.ts
@@ -14,6 +14,6 @@ export const name = 'recommended';
 export const config = {
   plugins: [pluginName],
   rules: {
-    [`${pluginName}/no-inheritance`]: 'error',
+    [`${pluginName}/no-class-inheritance`]: 'error',
   },
 };
