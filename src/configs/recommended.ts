5d5baaa35d67789306b7911dbca358dec2e17e76
Alexander <lsndrme@ya.ru>
2024-04-15 21:30:14 +0700

    refactor: reorganize code (#3)

--- src/configs/recommended.ts
+++ src/configs/recommended.ts
@@ -3,8 +3,14 @@ import { readPackage } from './../helpers';
 const pkg = readPackage();
 const pluginName = pkg.name.replace('eslint-plugin-', '');
 
+/**
+ * Name
+ */
 export const name = 'recommended';
 
+/**
+ * Config
+ */
 export const config = {
   plugins: [pluginName],
   rules: {
--- src/rules/no-inheritance/no-inheritance.rule.ts
+++ src/rules/no-inheritance/no-inheritance.rule.ts
@@ -1,13 +1,64 @@
 import { ESLintUtils } from '@typescript-eslint/utils';
 import { ClassType } from './class-type';
+import { NamedCreateRuleMeta } from '@typescript-eslint/utils/eslint-utils';
 
 const createRule = ESLintUtils.RuleCreator(
   () => `https://github.com/lsndr/eslint-plugin-typescript-inheritance`,
 );
 
+/**
+ * Rule name
+ */
 export const name = 'no-inheritance';
 
+/**
+ * Options
+ */
+type NoInheritanceOfAbstractClassesOptions = {
+  noInheritanceOfAbstractClasses: boolean;
+};
+type Options = [NoInheritanceOfAbstractClassesOptions];
+
+/**
+ * Default Options
+ */
+const defaultOptions: Options = [
+  {
+    noInheritanceOfAbstractClasses: false,
+  },
+];
+
+/**
+ * Meta
+ */
+const meta: NamedCreateRuleMeta<string, Options> = {
+  docs: {
+    description: 'Inheritance is not allowed.',
+  },
+  messages: {
+    inheritanceNotAllowed: 'Inheritance is not allowed.',
+  },
+  type: 'problem',
+  schema: [
+    {
+      type: 'object',
+      properties: {
+        noInheritanceOfAbstractClasses: {
+          type: 'boolean',
+        },
+      },
+      additionalProperties: false,
+    },
+  ],
+};
+
+/**
+ * Rule
+ */
 export const rule = createRule({
+  meta,
+  name,
+  defaultOptions,
   create(context, options) {
     return {
       ClassDeclaration(node) {
@@ -33,30 +84,4 @@ export const rule = createRule({
       },
     };
   },
-  meta: {
-    docs: {
-      description: 'Inheritance is not allowed.',
-    },
-    messages: {
-      inheritanceNotAllowed: 'Inheritance is not allowed.',
-    },
-    type: 'problem',
-    schema: [
-      {
-        type: 'object',
-        properties: {
-          noInheritanceOfAbstractClasses: {
-            type: 'boolean',
-          },
-        },
-        additionalProperties: false,
-      },
-    ],
-  },
-  name,
-  defaultOptions: [
-    {
-      noInheritanceOfAbstractClasses: false,
-    },
-  ],
 });
